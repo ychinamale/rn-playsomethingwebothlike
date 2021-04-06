585ff7599bde678624eb9b05007a8c0b91a6b3dd
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-06 17:11:20 +0200

    Import screens and export screens object

---
+++ App/navigation/screens.js
@@ -0,0 +1,6 @@
+// Screens with tabs on bottom tabbar
+import Home from '../screens/HomeScreen';
+import Playlists from '../screens/PlaylistScreen';
+import Matches from '../screens/MatchesScreen';
+
+export default { Playlists, Home, Matches };
