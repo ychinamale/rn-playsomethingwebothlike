3f7d592ef7b8c3961456898bfb6a5064c900add8
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-06 16:27:08 +0200

    Added function to check if token is valid

---
+++ App/utils/helpers.js
@@ -0,0 +1,13 @@
+export const isTokenValid = (tokenObj) => {
+  if (!tokenObj) return false;
+
+  const { access_token, expires_in, retrieved, token_type } = tokenObj;
+
+  if (expires_in == null ) return false;
+  if (retrieved == null) return false;
+  if (access_token == null || access_token.length === 0) return false;
+  if (token_type == null || token_type.length === 0) return false;
+  
+  const expiry = (expires_in - 300) * 1000;
+  return new Date().getTime() < (retrieved + expiry);
+}
