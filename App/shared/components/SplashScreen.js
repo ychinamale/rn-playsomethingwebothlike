c403126183539ad0d597182f1ca52ea22d5e3d18
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-06 18:05:44 +0200

    Added gif to the splash screen

--- App/shared/components/SplashScreen.js
+++ App/shared/components/SplashScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { ActivityIndicator, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function SplashScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.layout} style={{ flex: 1 }}>
+        <Image
+            style={{ width: 300, height: 300 }}
+            resizeMode="contain"
+            source={require('../../assets/music.gif')}
+        />
         <Text style={styles.text}>Play Something We Both Like</Text>
       </ScrollView>
     </SafeAreaView>
@@ -13,7 +18,7 @@ export default function SplashScreen() {
 
 const styles = StyleSheet.create({
   layout: {
-    alignItems: 'center', backgroundColor: 'green', flexGrow: 1, justifyContent: 'center',
+    alignItems: 'center', backgroundColor: 'white', flexGrow: 1, justifyContent: 'center',
     padding: 15,
   },
   text: {
