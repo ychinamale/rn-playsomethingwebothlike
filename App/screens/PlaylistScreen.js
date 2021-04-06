6b10757738634df3bc67e01e0731102cc770e4f9
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-06 17:10:25 +0200

    Dummy playlist screen

---
+++ App/screens/PlaylistScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function DUMMYSCREEN() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        PlaylistScreen, yea! 
+      </Text>
+    </View>
+  )
+
+}
