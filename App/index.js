56dbc71a997ba008f3e2203a55e626425c70ca9b
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-07 14:01:00 +0200

    Wrap nav in PlaylistContainer (context)

--- App/index.js
+++ App/index.js
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
 import InstanceInit from './instance';
 import MainNavigation from './navigation';
+import { PlaylistContainer } from './features/Playlists';
 
 export default function App() {
   return (
     <InstanceInit>
-      <MainNavigation />
+      <PlaylistContainer>
+        <MainNavigation />
+      </PlaylistContainer>
     </InstanceInit>
-  )
+  );
 }
