fa49988d7cec8ab2f9a7fb0a157f5d6d3926f2be
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-07 13:59:11 +0200

    Removed prefetching of token. Not useful here.

--- App/instance/index.js
+++ App/instance/index.js
@@ -1,45 +1,42 @@
 import React from 'react';
-import { getCachedTokenObj } from './cache';
-import { isTokenValid } from '../utils/helpers';
+import PropTypes from 'prop-types';
 import SplashScreen from '../shared/components/SplashScreen';
 
 export const InstanceContext = React.createContext();
 
 export default function InstanceInit({ children }) {
-
   const [appIsReady, setAppIsReady] = React.useState(false);
   const [token, setToken] = React.useState({});
 
   const intialise = async () => {
-    // attempt to retrieve token from cache
-    let tokenObj = await getCachedTokenObj();
-
-    // check if token is valid
-    if (isTokenValid(tokenObj)) {
-      setToken(tokenObj);
-      return null;
-    }
-
     setTimeout(() => {
       console.log('Disabling splash screen.');
       setAppIsReady(true);
     }, 2500);
-  }
 
-  React.useEffect(()=>{
+    return null;
+  };
+
+  React.useEffect(() => {
     intialise();
-  },[])
-
+  }, []);
 
   if (!appIsReady) {
     console.log('App is not ready yet. Show splash');
 
-    return <SplashScreen />
+    return <SplashScreen />;
   }
 
   return (
     <InstanceContext.Provider value={{ token, setToken }}>
       {children}
     </InstanceContext.Provider>
-  )
+  );
 }
+
+InstanceInit.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element),
+  ]).isRequired,
+};
