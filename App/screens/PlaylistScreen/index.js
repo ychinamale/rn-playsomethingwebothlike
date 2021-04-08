f446d5200fa2e84beb738cb9905dbaf315e8c4cb
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-08 08:28:31 +0200

    Replaced view with scrollview

--- App/screens/PlaylistScreen/index.js
+++ App/screens/PlaylistScreen/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import PlaylistsView from './PlaylistsView';
 
 export default function DUMMYSCREEN() {
   return (
-    <View style={{
-      flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center',
-    }}
+    <ScrollView
+      contentContainerStyle={[styles.layout, styles.centered]}
+      showsVerticalScrollIndicator={false}
+      style={{ flex: 1 }}
     >
-      <Text>
-        Playlist Screen, yea!
-      </Text>
       <PlaylistsView />
-    </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  expandButton: {
+    paddingVertical: 12,
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  layout: {
+    backgroundColor: 'white',
+    flexGrow: 1,
+    paddingVertical: 36,
+    paddingHorizontal: 12,
+  },
+  text: {
+    color: 'green',
+    fontSize: 16,
+    fontStyle: 'italic',
+    letterSpacing: 1.2,
+  },
+});
