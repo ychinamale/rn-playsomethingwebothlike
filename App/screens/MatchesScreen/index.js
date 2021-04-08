978b65b6808d6c57ca9f64e1d1e3a825b30c4409
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-08 08:28:56 +0200

    Replaced view with scrollview, set gif placeholder

--- App/screens/MatchesScreen/index.js
+++ App/screens/MatchesScreen/index.js
@@ -1,15 +1,50 @@
+/* eslint-disable global-require */
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Image, ScrollView, StyleSheet, Text, View,
+} from 'react-native';
 
 export default function DUMMYSCREEN() {
   return (
-    <View style={{
-      flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center',
-    }}
+    <ScrollView
+      contentContainerStyle={[styles.layout, styles.centered]}
+      showsVerticalScrollIndicator={false}
+      style={{ flex: 1 }}
     >
-      <Text>
-        Matches Screen, yea!
-      </Text>
-    </View>
+      <Image
+        resizeMode="cover"
+        style={styles.image}
+        source={require('../../assets/nothing-yet-green-500.gif')}
+      />
+      <View>
+        <Text style={styles.text}>Nothing to show... yet.</Text>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  centered: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  expandButton: {
+    paddingVertical: 12,
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  layout: {
+    backgroundColor: 'white',
+    flexGrow: 1,
+    paddingVertical: 36,
+    paddingHorizontal: 12,
+  },
+  text: {
+    color: 'green',
+    fontSize: 16,
+    fontStyle: 'italic',
+    letterSpacing: 1.2,
+  },
+});
