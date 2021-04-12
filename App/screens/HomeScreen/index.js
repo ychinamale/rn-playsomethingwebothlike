c554ce3b51182e1a8abd474a7273957f6f4a1f1c
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-12 13:23:55 +0200

    Removed placeholder text

--- App/screens/HomeScreen/index.js
+++ App/screens/HomeScreen/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import PlaylistForm from './components/PlaylistForm';
 
-export default function DUMMYSCREEN() {
+export default function HomeScreen() {
   return (
     <View style={{
       flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center',
     }}
     >
-      <Text>
-        HomeScreen, yea!
-      </Text>
       <PlaylistForm />
     </View>
   );
