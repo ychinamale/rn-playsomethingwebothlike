0db09bd01b2fcc4c9ea8a2eccdc2f212448aa7b0
Yamikani Chinamale <ychinamale@gmail.com>
2021-04-20 14:32:37 +0200

    Updated styling

--- App/screens/HomeScreen/index.js
+++ App/screens/HomeScreen/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PlaylistForm from './components/PlaylistForm';
+import { scaleFont, scaleSize } from '../../utils/scaling';
 
 export default function HomeScreen() {
   return (
-    <View style={{
-      flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center',
-    }}
-    >
-      <PlaylistForm />
+    <View style={styles.layout}>
+      <>
+        <View style={styles.headingLayout}>
+          <Text style={styles.headingText}>
+            Enter your Spotify playlists
+          </Text>
+        </View>
+        <PlaylistForm />
+      </>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  headingLayout: {
+    marginTop: scaleSize(48),
+  },
+  headingText: {
+    color: '#1DB954',
+    fontSize: scaleFont(20),
+    fontWeight: 'bold',
+    letterSpacing: 1.2,
+    textAlign: 'center',
+    textShadowColor: '#eee',
+    textShadowOffset: { width: scaleSize(2), height: scaleSize(4) },
+    textShadowRadius: 2,
+    height: scaleSize(48),
+  },
+});
